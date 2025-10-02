81599d9ac490c7cd9e99d877b75c392220b03512
James Aviani <james@hydrolix.io>
2025-10-02 08:13:41 -0700

    Updated bundle document.

---
+++ WIP/summary-zuplo/summaries/1_hour.sql
@@ -0,0 +1,21 @@
+SELECT
+  toStartOfHour (timestamp) AS hour,
+  customer,
+  payment_method,
+  status,
+  currency,
+  event_type,
+  COUNT(*) as event_count,
+  SUM(amount) as total_amount,
+  AVG(amount) as avg_amount,
+  MIN(timestamp) as first_event_time,
+  MAX(timestamp) as last_event_time
+FROM
+  __PROJECT_NAME__.__TABLE_NAME__
+GROUP BY
+  minute,
+  customer,
+  payment_method,
+  status,
+  currency,
+  event_type SETTINGS hdx_primary_key = 'hour'
