df41c4983d0896462ee56a99a077784a56817ec3
James Aviani <james@hydrolix.io>
2025-10-14 14:57:56 -0700

    Refactored deploy module

--- src/validate/summary_table.rs
+++ src/validate/summary_table.rs
@@ -3,21 +3,20 @@ use std::collections::HashSet;
 use crate::bundle_struct::Bundle;
 
 pub fn run(b: &Bundle) -> Result<(), String> {
-   
-        if b.summary_tables.is_none() {
-            return Ok(());
-        }
+    if b.summary_tables.is_none() {
+        return Ok(());
+    }
 
-        // Build set of valid table names from this bundle
-        let mut valid_tables: HashSet<String> = HashSet::new();
-        for t in &b.tables {
-            valid_tables.insert(t.name.clone());
-        }
+    // Build set of valid table names from this bundle
+    let mut valid_tables: HashSet<String> = HashSet::new();
+    for t in &b.tables {
+        valid_tables.insert(t.name.clone());
+    }
 
-        // Check each summary table references a valid parent
-        for st in b.summary_tables.as_ref().unwrap() {
-            if !valid_tables.contains(&st.parent_table_name) {
-                return Err(format!(
+    // Check each summary table references a valid parent
+    for st in b.summary_tables.as_ref().unwrap() {
+        if !valid_tables.contains(&st.parent_table_name) {
+            return Err(format!(
                     "ERROR: {}.{} Invalid-Parent-Table-Reference bundle={} summary_table={} parent_table_name={} url={}",
                     file!(),
                     line!(),
@@ -26,8 +25,8 @@ pub fn run(b: &Bundle) -> Result<(), String> {
                     st.parent_table_name,
                     b.base_url
                 ));
-            }
         }
+    }
 
     Ok(())
 }
