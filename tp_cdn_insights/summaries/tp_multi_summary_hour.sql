c56a069046b6b4c6d7841e4da75f40632b866b0a
Kevin Borkman <kevin.borkman@hydrolix.io>
2025-12-03 17:06:44 -0500

    Updated the repo name and added fixes for the new tp transform

--- tp_cdn_insights/summaries/tp_multi_summary_hour.sql
+++ tp_cdn_insights/summaries/tp_multi_summary_hour.sql
@@ -1,12 +1,12 @@
 SELECT
-  toStartOfHour (reqTimeSec) as reqTimeSec,
-  statusCode,
-  cacheStatus,
+  toStartOfHour (reqTimeSec) as timestamp,
+  statusCode as response_status_code,
+  cacheStatus as cache_was_cached,
   edge_pop,
-  reqHost,
-  country,
+  reqHost as request_host,
+  country as client_country_iso_code,
   city,
-  Edge_GeoInfo, -- this is client_asn // asn
+  Edge_GeoInfo as client_asn,
   user_agent_category,
   hdx_cdn,
   count() as cnt_all,
@@ -20,14 +20,14 @@ SELECT
 FROM
   __PROJECT_NAME__.__TABLE_NAME__
 GROUP BY
-  reqTimeSec,
-  statusCode,
-  cacheStatus,
+  timestamp,
+  response_status_code,
+  cache_was_cached,
   edge_pop,
-  reqHost,
-  country,
+  request_host,
+  client_country_iso_code,
   city,
-  Edge_GeoInfo,
+  client_asn,
   user_agent_category,
   hdx_cdn
-  SETTINGS hdx_primary_key = 'reqTimeSec'
+  SETTINGS hdx_primary_key = 'timestamp'
