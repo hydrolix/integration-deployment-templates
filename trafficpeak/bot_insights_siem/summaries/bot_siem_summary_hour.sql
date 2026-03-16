607963aa512e7bbf9a30872199f22da1523ba349
Kira Corbett <kcorbett@hydrolix.io>
2026-03-16 16:14:17 -0700

    test bundle

---
+++ trafficpeak/bot_insights_siem/summaries/bot_siem_summary_hour.sql
@@ -0,0 +1,17 @@
+SELECT
+  toStartOfHour (timestamp) AS timestamp,
+  bot_type,
+  bot_category,
+  resource_category,
+  attackTypes,
+  botScoreRange,
+  count() AS cnt_all
+FROM
+  akamai.siem
+GROUP BY
+  timestamp,
+  bot_type,
+  bot_category,
+  resource_category,
+  attackTypes,
+  botScoreRange SETTINGS hdx_primary_key = 'timestamp'
