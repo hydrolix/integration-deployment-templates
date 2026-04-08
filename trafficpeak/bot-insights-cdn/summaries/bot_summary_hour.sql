4491abf29d4a5079b0d550289a8fa8f78ec62ee2
Kira Corbett <kcorbett@hydrolix.io>
2026-04-08 12:08:09 -0700

    re-add assets

--- trafficpeak/bot-insights-cdn/summaries/bot_summary_hour.sql
+++ trafficpeak/bot-insights-cdn/summaries/bot_summary_hour.sql
@@ -1,32 +1,28 @@
 SELECT
-  toStartOfHour (timestamp) as timestamp,
-  cache_was_cached,
-  response_status_code,
-  request_host,
-  client_country_iso_code,
-  client_city,
-  client_asn,
-  edge_pop,
+  toStartOfHour (reqTimeSec) AS timestamp,
+  cacheStatus,
+  statusCode,
+  reqHost,
+  country,
+  city,
+  asn,
   user_agent_category,
   ai_category,
   resource_category,
-  hdx_cdn,
   is_bot_traffic,
-  count() as cnt_all,
-  sum(response_total_bytes) as response_total_bytes
+  count() AS cnt_all,
+  sum(totalBytes) AS totalBytes
 FROM
-  __PROJECT_NAME__.__TABLE_NAME__
+  akamai.logs
 GROUP BY
   timestamp,
-  cache_was_cached,
-  response_status_code,
-  request_host,
-  client_country_iso_code,
-  client_city,
-  client_asn,
-  edge_pop,
+  cacheStatus,
+  statusCode,
+  reqHost,
+  country,
+  city,
+  asn,
   user_agent_category,
   ai_category,
   resource_category,
-  is_bot_traffic,
-  hdx_cdn SETTINGS hdx_primary_key = 'timestamp'
+  is_bot_traffic SETTINGS hdx_primary_key = 'timestamp'
